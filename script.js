033010cfbcc137d7415cfc05aea8741c7737a188
Mukesh KUSHWAHA <101133241+mukku12345@users.noreply.github.com>
2022-08-10 02:00:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,150 @@
+// function dec(deci){
+//     var binary = document.getElementById('binary')
+//     var decimal = document.getElementById('decimal').value
+
+//     const binary = []
+
+//     if (decimal == 0){
+//         binary.value = ''
+//     }
+
+//     else{
+//         while(Math.floor(deci/2) >= 1){ 
+//             if(Math.floor(deci%2 == 0)){
+//                 binario.push(0)
+//             }
+//             else if(Math.floor(deci%2 == 1)){
+//                 binario.push(1)
+//             }
+//             deci = Math.floor(deci/2)
+//         }
+
+//         binario.push(1)
+//         binario.reverse()
+    
+//         saida.value = binario.join('')
+//     }
+// }
+// document.querySelector('.arrow').addEventListener('click', function(){
+//     document.querySelector('.menu').classList.toggle('active')
+//     document.querySelector('.arrow').classList.toggle('active')
+// })
+
+
+var  input =document.getElementById("input");
+var result =document.getElementById('result');
+var inputType =document.getElementById('inputType');
+var resultType = document.getElementById('resultType');
+var optionFrom,optionTo;
+
+//Adding EventListener
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+
+// Taking intial value
+
+optionFrom =inputType.value;
+optionTo  =resultType.value;
+
+function myResult(){
+
+    optionFrom =inputType.value;
+    optionTo= resultType.value;
+
+    // if(input.value==='')
+    // result.value='';
+
+if(optionFrom ==="decimal" && optionTo==="binary"){
+    // function dec2bin(dec){
+    //     return (dec >>> 0).toString(2);
+    //   } 
+    result.value =Number(input.value>>>0).toString(2)
+
+}else if(optionFrom==="decimal" && optionTo==="hexadecimal"){
+  result.value =Number(input.value>>>0).toString(16)
+ 
+
+}else if(optionFrom==="decimal" && optionTo==="octal"){
+    result.value =Number(input.value>>>0).toString(8)
+
+}
+else if(optionFrom==="decimal" && optionTo==="decimal"){
+    result.value =input.value;
+
+}
+
+//binary to decimal hex octal converting
+
+if(optionFrom==="binary" && optionTo==="decimal"){
+    result.value=parseInt(Number(input.value),2).toString(10)
+}else 
+if(optionFrom==="binary" && optionTo==="hexadecimal"){
+    result.value=parseInt(Number(input.value),2).toString(16)
+}else
+if(optionFrom==="binary" && optionTo==="octal"){
+    result.value=parseInt(Number(input.value),2).toString(8);
+ }
+ else
+if(optionFrom==="binary" && optionTo==="binary"){
+    result.value=input.value;
+}
+
+// hexadecimal to binary decimal octal converting
+
+
+if(optionFrom==="hexadecimal" && optionTo==="decimal"){
+    result.value=parseInt(String(input.value),16).toString(10);
+}else
+if(optionFrom==="hexadecimal" && optionTo==="octal"){
+    result.value=parseInt(String(input.value),16).toString(8);
+    
+}else
+if(optionFrom==="hexadecimal" && optionTo==="binary"){
+    result.value=parseInt(String(input.value),16).toString(2);
+
+    
+}else
+if(optionFrom==="hexadecimal" && optionTo==="hexadecimal"){
+    result.value=result.value;
+
+
+}
+
+
+//octal to decimal binary hexadecimal converting
+if(optionFrom==="octal" && optionTo==="decimal"){
+    result.value=parseInt(Number(input.value),8).toString(10);
+}else
+if(optionFrom==="octal" && optionTo==="hexadecimal"){
+    result.value=parseInt(Number(input.value),8).toString(16);
+    
+}else
+if(optionFrom==="octal" && optionTo==="binary"){
+    result.value=parseInt(Number(input.value),8).toString(2);
+    
+}else
+if(optionFrom==="octal" && optionTo==="octal"){
+    result.value=result.value;
+
+
+}
+}
+
+function refresh(){
+    window.location.reload("Refresh");
+    window.inputType.reload("Refresh");
+}
+
+function swap(){
+    var temp;
+    temp = inputType.value;
+    inputType.value =resultType.value;
+    resultType.value =temp;
+
+     var key;
+    key = input.value;
+    input.value =result.value;
+    result.value =key;
+}
